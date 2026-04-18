4a865815962d5d15087fcd5605834dc900c3b33b
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-04-18 16:49:17 +0300

    feat(validation): add umbrella header

---
+++ include/vix/validation/validation.hpp
@@ -0,0 +1,38 @@
+/**
+ *
+ *  @file validation/validation.hpp
+ *  @author Gaspard Kirira
+ *
+ *  @brief Internal aggregation header for the Vix validation module.
+ *
+ *  This file includes the core validation components provided by Vix,
+ *  including rules, schemas, validation results, error types, forms,
+ *  pipes, and base model helpers.
+ *
+ *  For most use cases, prefer:
+ *    #include <vix/validation.hpp>
+ *
+ *  Copyright 2025, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
+ */
+#ifndef VIX_VALIDATION_VALIDATION_HPP
+#define VIX_VALIDATION_VALIDATION_HPP
+
+#include <vix/validation/BaseModel.hpp>
+#include <vix/validation/Form.hpp>
+#include <vix/validation/Pipe.hpp>
+#include <vix/validation/Rule.hpp>
+#include <vix/validation/Rules.hpp>
+#include <vix/validation/Schema.hpp>
+#include <vix/validation/Validate.hpp>
+#include <vix/validation/ValidationError.hpp>
+#include <vix/validation/ValidationErrors.hpp>
+#include <vix/validation/ValidationResult.hpp>
+
+#endif // VIX_VALIDATION_VALIDATION_HPP
